151cf564397846d9a51ed0e077599f81fc55b051
opuR1 <lel070320@gmail.com>
2025-11-04 15:21:29 +0700

    Подключил бд

---
+++ sommer_al/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using sommer_al.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sommer_al
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FrmMain.Navigate(new Page1());
+        }
+
+        private void FrmMain_ContentRendered(object sender, EventArgs e)
+        {
+            if (FrmMain.CanGoBack)
+            {
+                btn_back.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                btn_back.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void btn_back_Click(object sender, RoutedEventArgs e)
+        {
+            FrmMain.GoBack();
+        }
+    }
+}
